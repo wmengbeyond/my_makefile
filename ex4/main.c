74e2da76c9f6f55c1010c1b0397b064a3f6451f7
wangmeng <wmengbeyond@163.com>
2014-12-24 22:45:58 +0800

    Makefile示例

---
+++ ex4/main.c
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+int main(int argc, char* argtv[])
+{
+    fn();
+    return 0;
+}
---
+++ ex5/base/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+
+void fn()
+{
+    printf("hello, Makefile!");
+}
